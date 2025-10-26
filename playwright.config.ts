0fd10e94b82f0492f799078216736224616a17b1
Pranav Pothiwala <146731593+TestRoverAutomation@users.noreply.github.com>
2025-10-26 15:46:30 +0000

    Cucmber multi reporter (#7)
    * Added cucmber report
    
    * Added cucmber report
    
    * updated cucumber multireport

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig, devices } from '@playwright/test';
-import { defineBddConfig } from 'playwright-bdd';
+import { defineBddConfig, cucumberReporter } from 'playwright-bdd';
 
 const testDir = defineBddConfig({
   features: 'tests/UI_Test/feature/***.feature',
@@ -20,14 +20,24 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html'], ['allure-playwright']],
+  reporter: [
+    ['html'],
+    ['allure-playwright'],
+    ['list'],
+    cucumberReporter('html', { outputFile: 'cucumber-report/index.html' }),
+    cucumberReporter('json', { outputFile: 'cucumber-report/results.json' }),
+  ],
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    trace: 'retain-on-failure',
+    timezoneId: 'UTC',
   },
 
   /* Configure projects for major browsers */
